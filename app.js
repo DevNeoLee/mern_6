e31c84d7b8a80cd370b48508701af656448bf736
DevNeoLee <devneolee@gmail.com>
2022-04-05 13:16:17 -0700

    Add route, model

---
+++ app.js
@@ -0,0 +1,39 @@
+const path = require('path');
+
+const http = require('http');
+// const socketio = require('socket.io');
+
+const db = require('./config/keys_dev').DB_URI
+
+const mongoose = require('mongoose')
+
+mongoose
+    .connect(db, { useNewUrlParser: true })
+    .then(() => console.log("Connected to MongoDB successfully"))
+    .catch(err => console.log(err));
+
+const express = require("express")
+const app = express()
+
+const res = require("express/lib/response")
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/build'));
+    app.get('/', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    })
+} else {
+    app.get('/', (req, res) => { return res.send("hello world from express")})
+}
+
+const bodyParser = require('body-parser')
+const game = require("./routes/api/game");
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+app.use("/api/game", game);
+
+const port = process.env.PORT || 5000
+
+app.listen(port, () => console.log(`Server is runnidng on the port ${port}, from express server`))
---
+++ config/keys_dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+    DB_URI: 'mongodb+srv://justin:Organize55@ground.d1j4r.mongodb.net/rpg_yu_group?retryWrites=true&w=majority',
+    secretOrKey: 'secret'
+}
