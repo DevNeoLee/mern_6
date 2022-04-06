c731c1e25a0166b9526014b709a10b2646a8ff5e
DevNeoLee <devneolee@gmail.com>
2022-04-06 00:00:49 -0700

    Add favicon Logo

--- frontend/src/components/Welcome.js
+++ frontend/src/components/Welcome.js
@@ -1,9 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-
 export default function Welcome() {
-  return (
-    <section>Welcome</section>
-  )
+    return (
+        <>
+            <h1>Welcome</h1>
+            <h2>...</h2>
+            <p>Lorem ipsum dolor sit amet,
+                consectetur adipiscing elit, sed do eiusmod tempor incididunt
+                ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                commodo consequat. Duis aute irure dolor in reprehenderit </p>
+            <Link to="/game1"><button>Enter</button></Link>
+        </>
+    )
 }
