43ddd07ccda297e231e35f173912d7c57a49a06b
DevNeoLee <devneolee@gmail.com>
2022-04-06 00:26:01 -0700

    Add Logo images

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import '../App.css'
 
 export default function Home() {
   return (
     <>
+      <header><div className="logo"><img src="/logo.png" /></div></header>
         <h1>Flood Game</h1>
         <h2>Welcome</h2>
+      <div className="logo"><img src="/favicon.png" /></div>
+
+      <div className="logo"><img src="/logo2.png" /></div>
           <p>Welcome to the RPG. Lorem ipsum dolor sit amet,
               consectetur adipiscing elit, sed do eiusmod tempor incididunt
               ut labore et dolore magna aliqua. Ut enim ad minim veniam,
